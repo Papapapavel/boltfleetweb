54e9236e54eb9dbb9145a616ad816c961f125ed9
Pavel Muzatko <pmuzatko@email.cz>
2025-03-11 16:48:42 +0100

    update

--- toto.js
+++ toto.js
@@ -1,3 +1,8 @@
 function scrollToSection() {
     document.getElementById("Kontakt").scrollIntoView({ behavior: "smooth" });
 }
+
+function scrollTovideo()
+{
+    document.getElementById("Taska").scrollIntoView({behavior: "smooth", block: "center"});
+}
